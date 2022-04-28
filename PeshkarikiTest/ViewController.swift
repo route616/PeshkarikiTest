18da0100247a59a4b1d8494e7d586c037986fb43
Igor <route616@icloud.com>
2022-04-28 20:34:19 +0300

    Initial Commit

---
+++ PeshkarikiTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeshkarikiTest
+//
+//  Created by Игорь on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
